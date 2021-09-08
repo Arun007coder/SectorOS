d830cd93f2b4334a6de8ee13d59a6483380d3dc7
Arun007coder <deepalali1007.agali@gmail.com>
2021-09-08 07:19:47 +0530

    * Changed the position of cursor * Added some comments to fetch_str.asm * Updated README.md

--- fetch_str.asm
+++ fetch_str.asm
@@ -2,15 +2,15 @@ mov es, bx
 mov bx, 0x0000
 
 read_HDD: ;;TODO:Read disk from a particular region
-    mov ah, 2
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov dl, 0x00
-    mov bx, 0x7E00
-    int 0x13
-    mov ah, 0x0E
-    mov al, [0x7E00]
-    int 0x10
-    ret
+    mov ah, 2       ; Read sector to memory
+    mov al, 1       ; Number of sectors to read
+    mov ch, 0       ; Cylinder
+    mov cl, 1       ; Sector
+    mov dh, 0       ; Head
+    mov dl, 0x00    ; Which drive to load
+    mov bx, 0x7E00  ; Offset
+    int 0x13        ; Disk service interrupt
+    mov ah, 0x0E    ; Print text to screen
+    mov al, [0x7E00]; Place where character is stored in memory
+    int 0x10        ; Video services interrupt
+    ret             ; Return to the main function
--- print_Keys.asm
+++ print_Keys.asm
@@ -1,20 +1,32 @@
-Buffer: times 10 db 'a'
-
 get_input: ;; TODO:Add Commands
-   mov ax, 0
-   int 0x16
-   mov bx, Buffer
-   mov [bx], al
-   inc bx
-   cmp al, 27
-   je shutdown
-   cmp al, 13
-   je print_Tex
-   cmp al, 9
-   je echo1
-   mov ah, 0x0e
-   int 0x10
-   jmp get_input
+   Buffer: times 10 db 0
+   shell:  db '$: ', 0
+   jmp setcursor
+   setcursor:
+      mov ah, 0x02   ; Set Cursor location
+      mov bh, 0x00   ; Page number
+      mov dh, 0x18   ; Row 24
+      mov dl, 0x00   ; Column 0
+      int     0x10
+      mov ah, 0x0E
+      mov bx, shell
+      call ptr_str
+      jmp input_loop
+   input_loop:
+      mov ax, 0
+      int 0x16
+      mov bx, Buffer
+      mov [bx], al
+      inc bx
+      cmp al, 27
+      je shutdown
+      cmp al, 13
+      je print_Tex
+      cmp al, 9
+      je echo1
+      mov ah, 0x0e
+      int 0x10
+      jmp input_loop
 
 clear_scr:
    mov ah, 0x00 ;; Set the graphical mode
@@ -34,15 +46,17 @@ echo1:
    jmp get_input
 
 print_Tex:
+   pusha
    mov ah, 0x0E
-   mov bx, Buffer 
+   mov bx, Buffer
    .lop:
       mov al, [bx]
-      cmp al, 13
-      je ex
       int 0x10
       add bx, 1
+      cmp al, 13
+      je ext
       jmp .lop
 
-ex:
-    jmp get_input
+   ext:
+      popa
+      jmp input_loop
